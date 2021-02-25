dbcd4198654c4950ff08cc587794e68de3683673
dahn0209 <dahn0209@gmail.com>
2021-02-25 01:56:34 -0500

    Athentication

--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 // const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dbStore = new SequelizeStore({ db: db });
+
+// sync so that our session table gets created
+dbStore.sync();
 
 // logging middleware
 app.use(morgan('dev'));
@@ -20,6 +27,36 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 //routes mounted on /api
 app.use('/api', require('./api'));
+
+//session middleware//
+// plug the store into our session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'a wildly insecure secret',
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Serialize/Deserialize User
+
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user.id);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => done(null, user))
+    .catch(done);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
